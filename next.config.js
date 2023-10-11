cfee77a38be5de8420db903499c2c0a25abd9d55
almohamady khalil <the.dev.syntax@gmail.com>
2023-10-11 15:40:26 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "raw.githubusercontent.com",
-  //       port: "",
-  //       pathname: "/the-dev-syntax/next-dave-final-project/main/images/**",
-  //     },
-  //   ],
-  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/the-dev-syntax/next-dave-final-project/main/images/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
